1a0837be3cf17dc26c7ae7907e94d353fa55bc6d
Alan Golart <alangolart@hotmail.com>
2020-07-14 21:58:38 +1000

    changing the server to launch a cluster

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
 const setupApp = require('./app')
 const config = require('./src/config/index')
 
 async function startServer() {
   try {
     const app = await setupApp()
-    app.listen(config.server.port || 3039)
-    console.log(`🚀 Listening on port ${config.server.port}`)
+    if (cluster.isMaster) {
+      console.log(`Master ${process.pid} is running`)
+      for (let i = 0; i < numCPUs; i += 1) {
+        cluster.fork()
+      }
+      cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`)
+      })
+    } else {
+      app.listen(config.server.port || 3030)
+      console.log(`🚀 Listening on port ${config.server.port}`)
+      console.log(`Worker ${process.pid} started`)
+    }
   } catch (error) {
     console.error(error)
   }
