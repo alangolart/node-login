bb92ac1bfdd41b616763e63f3fb3535d44ae39a2
Alan Golart <alangolart@hotmail.com>
2020-07-16 21:14:41 +1000

    adding validation on register

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
+const { registerValidation, validate } = require('../helpers/validator')
 
-router.post('/register', userController.register)
+router.post('/register', registerValidation(), validate, userController.register)
 
 module.exports = router
